76821e82e316c0c3d6e93e85981f451df6765bde
wyatt4543 <w4543w@gmail.com>
2026-03-02 14:41:24 -0600

    update bounds function to test for going off of the left side of the board

--- Pack It Up/Assets/Scripts/BoardScript.cs
+++ Pack It Up/Assets/Scripts/BoardScript.cs
@@ -40,7 +40,7 @@ public class BoardScript : MonoBehaviour
     // true means outside of bounds
     public bool TestOutside(int x, int y, int xUpdates = 0, int yUpdates = 0) {
         // if it is outside of bounds of 10 x 20 box return true
-        if ((x + xUpdates > 9) || (y + yUpdates > 19))
+        if ((x + xUpdates > 9) || (x + xUpdates < 0) || (y + yUpdates > 19))
         {
             return true;
         }

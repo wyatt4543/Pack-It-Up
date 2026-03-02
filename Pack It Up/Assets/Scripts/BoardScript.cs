f2b226fddb2a2d59597c5845a83c4d59e1762658
wyatt4543 <w4543w@gmail.com>
2026-03-02 14:55:57 -0600

    update print statements to be more detailed

--- Pack It Up/Assets/Scripts/BoardScript.cs
+++ Pack It Up/Assets/Scripts/BoardScript.cs
@@ -40,14 +40,15 @@ public class BoardScript : MonoBehaviour
     // true means outside of bounds
     public bool TestOutside(int x, int y, int xUpdates = 0, int yUpdates = 0) {
         // if it is outside of bounds of 10 x 20 box return true
-        print(y+yUpdates);
-        PrintBoard();
+        print("block position: {" + x + ", " + y + "}");
         if ((x + xUpdates > 9) || (x + xUpdates < 0) || (y + yUpdates > 19))
         {
+            print("block in bounds");
             return true;
         }
         //otherwise return false
         else {
+            print("block out of bounds");
             return false;
         }
     }

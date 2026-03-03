b39fabf5a0d42a58abe32e5be6cf33c0251ecec9
wyatt4543 <w4543w@gmail.com>
2026-03-03 01:00:22 -0600

    make the spawn function

--- Pack It Up/Assets/Scripts/SpawnBlock.cs
+++ Pack It Up/Assets/Scripts/SpawnBlock.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class SpawnBlock : MonoBehaviour
 {
+    public GameObject[] Blocks;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // spawn a new block at the beginning of the game
+        NewBlock();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // function for creating new blocks
+    public void NewBlock() {
+        // create a random new block in the spawner postion
+        Instantiate(Blocks[Random.Range(0, Blocks.Length)], transform.position, Quaternion.identity);
     }
 }

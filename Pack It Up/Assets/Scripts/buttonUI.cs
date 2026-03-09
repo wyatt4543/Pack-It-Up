56d7baf78cc55c3a911796eef4944f7db63ec2f2
wyatt4543 <w4543w@gmail.com>
2026-03-08 19:29:57 -0500

    add the functionality for the restart button

--- Pack It Up/Assets/Scripts/buttonUI.cs
+++ Pack It Up/Assets/Scripts/buttonUI.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class buttonUI : MonoBehaviour
 {
+    // set the string to the main game scene name
+    [SerializeField] private string mainGame = "MainGame";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // function for adding the behavior of restarting the game
+    public void RestartButton() {
+        SceneManager.LoadScene(mainGame);
     }
 }

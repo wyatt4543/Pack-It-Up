197fe7bb3790e1b905b363789a939861cc7e6494
wyatt4543 <w4543w@gmail.com>
2026-03-08 20:26:35 -0500

    create the functionality for the play button, settings button, and quit button

--- Pack It Up/Assets/Scripts/buttonUI.cs
+++ Pack It Up/Assets/Scripts/buttonUI.cs
@@ -19,6 +19,29 @@ public class buttonUI : MonoBehaviour
         }
     }
 
+    // function for adding the behavior of starting the game
+    public void PlayButton()
+    {
+        // load the main game on play
+        SceneManager.LoadScene(mainGame);
+    }
+
+    // function for opening the settings menu
+    public void SettingsButton()
+    {
+        // hide the other two buttons
+        ToggleButton(0);
+        ToggleButton(2);
+        
+        // open the settings menu 
+    }
+
+    // function for the button to quit the game
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
     // function for adding the behavior of restarting the game
     public void RestartButton() {
         // load the main game on restart

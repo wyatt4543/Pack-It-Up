f64f1d292b771f563e73dd7964f0932853c667fe
wyatt4543 <w4543w@gmail.com>
2026-03-08 21:30:04 -0500

    create the SFXManager script

---
+++ Pack It Up/Assets/Scripts/Controllers & Managers/SFXManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public static SFXManager instance;
+
+    [SerializeField] private AudioSource SFXObject;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlaySFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        //spawn in gameObject
+        AudioSource audioSource = Instantiate(SFXObject, spawnTransform.position, Quaternion.identity);
+
+        //assign the audioClip
+        audioSource.clip = audioClip;
+
+        //assign volume
+        audioSource.volume = volume;
+
+        //play sound
+        audioSource.Play();
+
+        //get length of SFX clip
+        float clipLength = audioSource.clip.length;
+
+        //destroy the clip after it is done playing
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    public void PlayRandomSFXClip(AudioClip[] audioClip, Transform spawnTransform, float volume)
+    {
+        //assign a random index
+        int rand = Random.Range(0, audioClip.Length);
+
+        //spawn in gameObject
+        AudioSource audioSource = Instantiate(SFXObject, spawnTransform.position, Quaternion.identity);
+
+        //assign the audioClip
+        audioSource.clip = audioClip[rand];
+
+        //assign volume
+        audioSource.volume = volume;
+
+        //play sound
+        audioSource.Play();
+
+        //get length of SFX clip
+        float clipLength = audioSource.clip.length;
+
+        //destroy the clip after it is done playing
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    public void PlayPitchedSFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        //spawn in gameObject
+        AudioSource audioSource = Instantiate(SFXObject, spawnTransform.position, Quaternion.identity);
+
+        //assign the audioClip
+        audioSource.clip = audioClip;
+
+        //assign pitch
+        audioSource.pitch = Random.Range(1f, 1.5f);
+
+        //assign volume
+        audioSource.volume = volume;
+
+        //play sound
+        audioSource.Play();
+
+        //get length of SFX clip
+        float clipLength = audioSource.clip.length;
+
+        //destroy the clip after it is done playing
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}

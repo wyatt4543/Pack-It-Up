a60e938fbd778e8cf078f2b08c513250e4674b35
wyatt4543 <w4543w@gmail.com>
2026-03-08 21:54:59 -0500

    update the menu manager script

--- Pack It Up/Assets/Scripts/Controllers & Managers/MenuManager.cs
+++ Pack It Up/Assets/Scripts/Controllers & Managers/MenuManager.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool menuOpen = false;
+    private GameObject soundMenu;
+    private SoundMixerManager soundMixer;
+    private Slider masterVolume;
+    private Slider SFXVolume;
+    private Slider musicVolume;
+    private Button backButton;
+
+    private void Start()
     {
-        
+        soundMenu = GameObject.Find("SoundMenuCanvas");
+        soundMenu.SetActive(false);
+        soundMixer = GameObject.Find("SoundMixerManager").GetComponent<SoundMixerManager>();
+        masterVolume = soundMenu.transform.Find("Master Volume").gameObject.GetComponent<Slider>();
+        SFXVolume = soundMenu.transform.Find("SFX Volume").gameObject.GetComponent<Slider>();
+        musicVolume = soundMenu.transform.Find("Music Volume").gameObject.GetComponent<Slider>();
+        backButton = soundMenu.transform.Find("BackButton").gameObject.GetComponent<Button>();
+        masterVolume.onValueChanged.AddListener(
+            delegate { soundMixer.SetMasterVolume(masterVolume.value); }
+        );
+        SFXVolume.onValueChanged.AddListener(
+            delegate { soundMixer.SetSFXVolume(SFXVolume.value); }
+        );
+        musicVolume.onValueChanged.AddListener(
+            delegate { soundMixer.SetMusicVolume(musicVolume.value); }
+        );
+        backButton.onClick.AddListener(
+            delegate { UpdateSoundMenu(); }
+        );
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            UpdateSoundMenu();
+        }
+    }
+
+    public void UpdateSoundMenu()
+    {
+        menuOpen = !menuOpen;
+        soundMenu.SetActive(menuOpen);
     }
 }

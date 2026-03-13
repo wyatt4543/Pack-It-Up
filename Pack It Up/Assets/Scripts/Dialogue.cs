5cc9ac75814d197c0a68bb6981f715bc8efe4971
wyatt4543 <w4543w@gmail.com>
2026-03-13 00:30:52 -0500

    adjust the names of anything related to dialogue & partially code the dialogue handler

--- Pack It Up/Assets/Scripts/Dialogue.cs
+++ Pack It Up/Assets/Scripts/Dialogue.cs
@@ -1,7 +1,14 @@
+using TMPro;
 using UnityEngine;
 
-public class Dialog : MonoBehaviour
+public class Dialogue : MonoBehaviour
 {
+    public TextMeshProUGUI textComponent;
+    public string[] dialogLines;
+    public float textSpeed;
+
+    private int index;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +20,6 @@ public class Dialog : MonoBehaviour
     {
         
     }
+
+    void StartDialogue
 }

9df5009094586ffffed1378c3ef23d661e94e78c
wyatt4543 <w4543w@gmail.com>
2026-03-13 23:44:37 -0500

    switch the input system for the pause manager

--- Pack It Up/Assets/Scripts/Controllers & Managers/PauseManager.cs
+++ Pack It Up/Assets/Scripts/Controllers & Managers/PauseManager.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PauseManager : MonoBehaviour
 {
     public static PauseManager instance;
-    
+    private PlayerInput playerInput;
+
     public bool IsPaused {  get; private set; }
 
     public void Awake()
@@ -12,17 +14,28 @@ public class PauseManager : MonoBehaviour
         {
             instance = this;
         }
+
+        // Initialize player input
+        playerInput = GetComponent<PlayerInput>();
     }
 
     public void PauseGame()
     {
+        // pause time
         IsPaused = true;
         Time.timeScale = 0f;
+
+        // switch the action map
+        playerInput.SwitchCurrentActionMap("UI");
     }
-    
+
     public void UnpauseGame()
     {
+        // unpause time
         IsPaused = false;
         Time.timeScale = 1f;
+
+        // switch the action map
+        playerInput.SwitchCurrentActionMap("Player");
     }
 }

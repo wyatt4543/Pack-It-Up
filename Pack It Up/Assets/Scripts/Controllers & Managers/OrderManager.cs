1c296814677140179c552fe96c8a871bb87b71c2
wyatt4543 <w4543w@gmail.com>
2026-03-15 17:50:06 -0500

    move the set active false back

--- Pack It Up/Assets/Scripts/Controllers & Managers/OrderManager.cs
+++ Pack It Up/Assets/Scripts/Controllers & Managers/OrderManager.cs
@@ -28,9 +28,8 @@ public class OrderManager : MonoBehaviour
             totalOrders = currentTotalOrders = 2;
             currentOrderText.enabled = false;
             totalOrdersText.enabled = false;
+            gameObject.SetActive(false);
         }
-
-        gameObject.SetActive(false);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created

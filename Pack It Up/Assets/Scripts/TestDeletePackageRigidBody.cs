fe0a9b722fed03e73cde68b378a3670098fe6ef8
wyatt4543 <w4543w@gmail.com>
2026-03-22 01:42:24 -0500

    make the code for playing the worker throw animation

--- Pack It Up/Assets/Scripts/TestDeletePackageRigidBody.cs
+++ Pack It Up/Assets/Scripts/TestDeletePackageRigidBody.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class TestDeletePackageRigidBody : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D packageRigidBody;
+    private Animator workerAnimator;
+
+    private void Awake()
+    {
+        workerAnimator = GameObject.Find("Worker").GetComponent<Animator>();
+    }
 
     // delete the rigidbody2d of this package upon entering a trigger
     private void OnTriggerEnter2D(Collider2D other)
@@ -11,5 +17,8 @@ public class TestDeletePackageRigidBody : MonoBehaviour
         
         // snap the package into place
         transform.position = new Vector2(transform.position.x, 21.625f);
+
+        // make the worker throw the package
+        workerAnimator.SetTrigger("Throw");
     }
 }

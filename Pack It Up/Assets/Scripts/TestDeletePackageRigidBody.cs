cf8cea3d57d493484e49a94b646f0212bea921ce
wyatt4543 <w4543w@gmail.com>
2026-03-22 01:06:14 -0500

    make a script to delete the package rigid body upon entering a trigger

---
+++ Pack It Up/Assets/Scripts/TestDeletePackageRigidBody.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class TestDeletePackageRigidBody : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D packageRigidBody;
+
+    // delete the rigidbody2d of this package upon entering a trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(packageRigidBody);
+    }
+}

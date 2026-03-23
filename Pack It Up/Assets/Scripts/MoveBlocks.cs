f720f78a50f8093d336dff2f010e4e3ca5a4c042
wyatt4543 <w4543w@gmail.com>
2026-03-22 19:18:48 -0500

    center the drag block to the mouse

--- Pack It Up/Assets/Scripts/MoveBlocks.cs
+++ Pack It Up/Assets/Scripts/MoveBlocks.cs
@@ -287,7 +287,7 @@ public class MoveBlocks : MonoBehaviour
         // update the block position if it is valid
         if (ValidMove(mouseXMovement, 0))
         {
-            parentTransform.position = new Vector2(Mathf.RoundToInt(worldPosition.x), parentTransform.position.y);
+            parentTransform.position = new Vector2(Mathf.RoundToInt(worldPosition.x) + 1.5f, parentTransform.position.y);
         }
     }
 

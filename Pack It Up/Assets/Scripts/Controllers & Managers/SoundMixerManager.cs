193c8441d0371ef69ff17f505dd3bfc16e84f024
wyatt4543 <w4543w@gmail.com>
2026-03-24 00:18:36 -0500

    Revert "make there only be one instance of the soundmixermanager"
    This reverts commit 111f9c22e1bd35bbdc2b067d7ce0a6b10fac597e.

--- Pack It Up/Assets/Scripts/Controllers & Managers/SoundMixerManager.cs
+++ Pack It Up/Assets/Scripts/Controllers & Managers/SoundMixerManager.cs
@@ -3,21 +3,11 @@ using UnityEngine.Audio;
 
 public class SoundMixerManager : MonoBehaviour
 {
-    private static SoundMixerManager instance;
     private AudioMixer audioMixer;
 
     private void Awake()
     {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-            audioMixer = Resources.Load<AudioMixer>("Sounds/MainMixer");
-        }
-        else
-        {
-            Destroy(gameObject); // Destroy duplicates
-        }
+        audioMixer = Resources.Load<AudioMixer>("Sounds/MainMixer");
     }
 
     public void SetMasterVolume(float level)

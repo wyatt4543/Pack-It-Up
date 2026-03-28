99a7cc0e16db4ffa30eeb69d23be6571b0f55427
wyatt4543 <w4543w@gmail.com>
2026-03-28 07:31:10 -0500

    undo the change

--- Pack It Up/Assets/Scripts/MoveBlocks.cs
+++ Pack It Up/Assets/Scripts/MoveBlocks.cs
@@ -350,6 +350,9 @@ public class MoveBlocks : MonoBehaviour
                 
                 // move the rows down
                 RowDown(i);
+
+                // double check the row
+                i++;
             }
         }
 

9528f71c313d8cbf2019f4e4c54d789cc6686c4c
wyatt4543 <w4543w@gmail.com>
2026-03-29 17:24:59 -0500

    remove the got changed variable

--- Pack It Up/Assets/Scripts/Controllers & Managers/PauseManager.cs
+++ Pack It Up/Assets/Scripts/Controllers & Managers/PauseManager.cs
@@ -9,7 +9,6 @@ public class PauseManager : MonoBehaviour
     public bool levelComplete = false;
 
     public bool IsPaused {  get; private set; }
-    public bool GotChanged { get; private set; }
 
     public void Awake()
     {
@@ -21,31 +20,21 @@ public class PauseManager : MonoBehaviour
 
     public void PauseGame()
     {
-        // tell other functions that the value of isPaused has changed
-        GotChanged = true;
-
         // pause time
         IsPaused = true;
         Time.timeScale = 0f;
 
         // switch the action map
         playerInput.SwitchCurrentActionMap("UI");
-
-        GotChanged = false;
     }
 
     public void UnpauseGame()
     {
-        // tell other functions that the value of isPaused has changed
-        GotChanged = true;
-
         // unpause time
         IsPaused = false;
         Time.timeScale = 1f;
 
         // switch the action map
         playerInput.SwitchCurrentActionMap("Player");
-
-        GotChanged = false;
     }
 }

d9127c6bb1bf8f45c43e47d9852472c3a4aa903c
wyatt4543 <w4543w@gmail.com>
2026-03-31 15:31:26 -0500

    add a null check

--- Pack It Up/Assets/Scripts/SpawnBlock.cs
+++ Pack It Up/Assets/Scripts/SpawnBlock.cs
@@ -38,18 +38,21 @@ public class SpawnBlock : MonoBehaviour
 
     // function for creating new blocks
     public async void NewBlock(bool fromHandleBlockPlacement = false, int blockType = -1) {
-        
+
         // make sure handle block placement does not place a second block
-        if (MoveBlocks.instance.currentBlock.gameObject.name == "JNegativeBlock")
+        if (MoveBlocks.instance != null)
         {
-            if (!fromHandleBlockPlacement)
+            if (MoveBlocks.instance.currentBlock.gameObject.name == "JNegativeBlock")
             {
-                // destroy the negative block
-                Destroy(MoveBlocks.instance.currentBlock.transform.parent.gameObject);
-            }
-            else
-            {
-                return;
+                if (!fromHandleBlockPlacement)
+                {
+                    // destroy the negative block
+                    Destroy(MoveBlocks.instance.currentBlock.transform.parent.gameObject);
+                }
+                else
+                {
+                    return;
+                }
             }
         }
 

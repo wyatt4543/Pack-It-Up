d35033d3206166f5407411d1cc0dc1785869d84e
wyatt4543 <w4543w@gmail.com>
2026-03-31 13:00:26 -0500

    stop the new block from being made twice

--- Pack It Up/Assets/Scripts/MoveBlocks.cs
+++ Pack It Up/Assets/Scripts/MoveBlocks.cs
@@ -244,7 +244,7 @@ public class MoveBlocks : MonoBehaviour
                 parentTransform.Translate(Vector2.down);
             }
             // once it hits the bottom of the screen place the block
-            else if (!placingBlock && !isClearing)
+            else if (!placingBlock && !isClearing && !NegativeBlockCalled)
             {
                 placingBlock = true;
                 _ = HandleBlockPlacement();
@@ -270,7 +270,7 @@ public class MoveBlocks : MonoBehaviour
         SFXManager.instance.PlayPitchedSFXClip(placeSound, transform, 1f);
 
         // check for negative block to stop anything from being added
-        if (currentBlock.gameObject.name != "JNegativeBlock" && currentBlock == null)
+        if (currentBlock.gameObject.name != "JNegativeBlock")
         {
             // add the block to the grid
             AddToGrid();
@@ -1163,7 +1163,7 @@ public class MoveBlocks : MonoBehaviour
     private void NegativeBlockDestruction()
     {
         // check if this function has been called
-        if (NegativeBlockCalled == false)
+        if (NegativeBlockCalled == false && placingBlock == false)
         {   
             // stop this from spawning multiple blocks
             NegativeBlockCalled = true;
